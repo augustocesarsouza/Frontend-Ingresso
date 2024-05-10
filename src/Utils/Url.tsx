4495f18305233492121b6cdb0a52d34280bd0e74
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-05-10 08:30:41 -0400

    alterando url

--- src/Utils/Url.tsx
+++ src/Utils/Url.tsx
@@ -1,3 +1,3 @@
 // export const Url = 'https://localhost:6556/v1'; //c#
 //export const Url = 'http://localhost:5000/v1'; python
-export const Url = 'http://18.217.75.125:8080/v1';
+export const Url = 'http://18.118.50.53:8080/v1';
