55ef4217521fd519cb36c548dd0031f0107f7b85
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-05-10 06:32:29 -0400

    alterando vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 6400,
-  }
+  base: '/deploy-vite/'
+  // server: {
+  //   port: 6400,
+  // }
   // preview: {
   //   host: true,
   //   port: 3050,
