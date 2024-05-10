a50a1f8a2eb92ac1536c9568d9a3b852633705f4
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-05-10 07:07:59 -0400

    configuração de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Frontend-Ingresso/'
+  base: '/Frontend-Ingresso'
   // server: {
   //   port: 6400,
   // }
